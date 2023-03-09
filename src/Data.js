95b9dff358f282929350726c3ab675585ebcd6fc
Diaz1080 <Nydia1080@yahoo.com>
2023-03-08 19:39:48 -0500

    changes

--- src/Data.js
+++ src/Data.js
@@ -4,20 +4,20 @@ export default [
 	  title: "Weather App",
 	  text: "First frontend project modeled after a weather app.",
 	  button: "Let the forecast shine☀️",
-	  link: "/project/0",
+	  link: "/project/Weather",
 	},
 	{
 	  img: "https://portfolio.maxmatthe.ws/feedbackForm.png",
 	  title: "Feedback form",
 	  text: "Feedback form modeled after Google Forms.",
 	  button: "Get Feedback",
-	  link: "/project/Feedback",
+	  link: "/project/feedback",
 	},
 	{
 	  img: "https://fontmeme.com/permalink/221025/e32c8b0cb8655f593d53e821877e03e2.png",
 	  title: "Aflix",
 	  text: "My first project! Recreation of a particular streaming service.",
 	  button: "Get Streaming",
-	  link: "./project/Aflix",
+	  link: "../project/Aflix",
 	},
   ];
